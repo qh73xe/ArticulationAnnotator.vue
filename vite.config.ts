e7b3821637c9de672681b42f42d667989b0cda05
qh73xe <takuya.waseda.1119@gmail.com>
2023-11-12 19:13:39 +0900

    :+1: #main @qh73xe : eslint, prettierrc, husky の設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+import vue from '@vitejs/plugin-vue';
+import checker from 'vite-plugin-checker';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig((env) => ({
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/main.ts'),
+      name: 'articulationannotator.vue',
+      fileName: 'articulationannotator.vue.lib',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: { globals: { vue: 'Vue' } },
+    },
+  },
+  plugins: [
+    dts(),
+    vue(),
+    env.mode !== 'test' &&
+      checker({
+        vueTsc: true,
+        eslint: { lintCommand: 'eslint "./src/**/*.{ts,vue}"' },
+      }),
+  ],
+}));
